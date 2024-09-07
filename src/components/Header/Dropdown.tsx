ea4169dc904b1b1c53e1430c2a7f145b45063096
HappyRio <happyrio1013@gmail.com>
2024-09-08 04:04:45 +0900

    home page

---
+++ src/components/Header/Dropdown.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+import type { Selection } from "@nextui-org/react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+
+export default function HeaderDropdown() {
+  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set(["Maximum Visiblity"]));
+
+  const selectedValue = React.useMemo(
+    () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
+    [selectedKeys]
+  );
+
+  return (
+    <Dropdown >
+      <DropdownTrigger>
+        <Button
+          variant="bordered" 
+          className="text-nowrap"
+        >
+          <LightModeOutlinedIcon/>{selectedValue}
+        </Button>
+        {/* <KeyboardArrowDownIcon/> */}
+      </DropdownTrigger>
+      <DropdownMenu 
+        className="flex flex-col"
+        aria-label="Single selection example"
+        variant="flat"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKeys={selectedKeys}
+        onSelectionChange={setSelectedKeys}
+      >
+        <DropdownItem key="Maximum Visiblity"><LightModeOutlinedIcon/>Maximum Visiblity</DropdownItem>
+        <DropdownItem key="Privacy Focused"><WbSunnyOutlinedIcon/>Privacy Focused</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Image from "next/image";
+import { Input } from "@nextui-org/input";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import HeaderDropdown from "./Dropdown";
+
+export default function Header() {
+  return (
+    <>
+      <div className="flex justify-between items-center px-4 mx-auto 1md:block pl-16 py-4 h-[76px] gap-8">
+        <div>
+          <Image src={"/logo.png"} alt="" width={108} height={40} />
+        </div>
+        <div className="flex w-full relative">
+              <SearchOutlinedIcon className="absolute pl-2 top-1/2 text-3xl 1translate-x-1/2 -translate-y-1/2" />
+              <input className="flex w-full p-2 pl-10  rounded-full bg-gray-200 outline-blue-200 outline-8" aria-autocomplete="both" aria-labelledby="autocomplete-17-label" id="autocomplete-17-input" placeholder="Search jobs, companies, and more..." type="search"></input>
+        </div>
+        <div className="flex items-center justify-end pl-2 ml-auto space-x-2 xl:col-span-1">
+          <HeaderDropdown/>
+          <NotificationsNoneOutlinedIcon/>
+          <MapsUgcOutlinedIcon/>
+          <div className="w-8 h-8">
+            <div className="flex items-center justify-center w-full h-full overflow-hidden text-lg font-semibold text-center text-gray-600 uppercase bg-gray-200 rounded-full group-hover:bg-gray-400 dark:group-hover:bg-gray-900 dark:bg-gray-700 dark:text-gray-200 ng-star-inserted">
+              {" "}
+              Y{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
